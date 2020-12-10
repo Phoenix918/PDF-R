b2110186dd0d0a8a7bc1e986e10a2532af8b45cf
Phoenix918 <66797007+Phoenix918@users.noreply.github.com>
2020-12-10 19:13:17 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'Task-3/task3.dart';
+
+main() {
+  runApp(H_Reload());
+}
+
+@override
+class H_Reload extends StatelessWidget {
+  build(BuildContext b1) {
+    return task3();
+  }
+}
